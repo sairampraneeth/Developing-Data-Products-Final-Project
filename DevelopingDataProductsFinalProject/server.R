c62f74143b33c6e2d7dd9ee3431f393d014634a8
Praneeth <sairampraneeth@gmail.com>
2016-10-09 16:34:03 -0500

    almost final

---
+++ DevelopingDataProductsFinalProject/server.R
@@ -0,0 +1,53 @@
+library(shiny)
+data(mtcars)
+
+Dset <- mtcars
+PredictionModel <- lm(mpg ~ .,data = Dset)
+checker <- TRUE
+
+shinyServer(
+  function(input,output){
+    printEvent <- eventReactive(input$button1,{
+      test <- data.frame(cyl = input$id1,
+                         disp = input$id2,
+                         hp = input$id3,
+                         drat = input$id4,
+                         wt = input$id5,
+                         qsec = input$id6,
+                         vs = switch(input$id7,V = 0,S = 1),
+                         am = switch(input$id8,A = 0,M = 1),
+                         gear = input$id9,
+                         carb = input$id10
+      )
+      predmod <<- PredictionModel
+      print(paste("The Predicted Mileage is",predict(predmod,test)))
+    })
+    observeEvent(input$button2,{
+      test <- data.frame(cyl = input$id1,
+                         disp = input$id2,
+                         hp = input$id3,
+                         drat = input$id4,
+                         wt = input$id5,
+                         qsec = input$id6,
+                         vs = switch(input$id7,V = 0,S = 1),
+                         am = switch(input$id8,A = 0,M = 1),
+                         gear = input$id9,
+                         carb = input$id10
+      )
+      predmod <<- PredictionModel
+      pred <- predict(predmod,test)
+      newRecord <- data.frame(mpg = pred,test)
+      Dset <<- rbind(Dset,newRecord)
+      PredictionModel <<- lm(mpg ~ .,data = Dset)
+    })
+    output$predictedvalue <- renderText({
+      printEvent()
+    })
+    actionDone <- eventReactive(input$button2,{
+      print(paste("New Record Added!! Total Number of Records = ",nrow(Dset)))
+    })
+    output$ActionDone <- renderText({
+      actionDone()
+    })
+  }
+)
---
+++ DevelopingDataProductsFinalProject/ui.R
@@ -0,0 +1,46 @@
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    title = "Car Mileage Prediction using MTcars Database",
+    fluidRow(
+      column(width = 2,
+             numericInput("id4","Rear Axle Ratio",2.5,min = 2,max = 5,step = 0.1),
+             numericInput("id5","Weight",3,min = 1,max = 6,step = 0.1),
+             numericInput("id6","quarter mile time",20,min = 10,max = 30,step = 0.1),
+             numericInput("id2","Displacement",100,min = 50,max = 600,step = 10),
+             numericInput("id3","Horse Power",130,min = 50,max = 350,step = 10)
+      ),
+      column(width = 3,
+             radioButtons("id7","Engine Type",c(
+               "V" = "V",
+               "S" = "S"
+             )),
+             radioButtons("id8","Transmission Type",c(
+               "Automatic" = "A",
+               "Manual" = "M"
+             ))  
+      ),
+      column(width = 4,
+             sliderInput("id1","Number of Cylinders",4,8,6,step = 1),
+             sliderInput("id9","Number of Gears",3,5,4,step = 1),
+             sliderInput("id10","Number of Carburetors",1,8,5,step = 1)     
+      ),
+      column(width = 4, offset = 5,
+             br(),
+             actionButton("button1","Predict Mileage")
+      ),
+      column(width = 5, offset = 5,
+             br(),
+             actionButton("button2","Add New Record")
+      ),
+      column(width = 5, offset = 6,
+             textOutput("ActionDone")
+      ),
+      mainPanel(
+        h1("Mileage Prediction"),
+        textOutput("predictedvalue")
+      )
+    )
+  )
+)
